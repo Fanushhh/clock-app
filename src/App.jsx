c1fe87176e512d7983a31afc88cb6d0c0c6c505f
Fanush <fgabriel.mihailescu@gmail.com>
2024-09-03 18:42:31 +0300

    feat: added details

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import "./App.css";
-
+import {Quote} from "./components/Quote";
+import {Clock} from "./components/Clock";
 function App() {
+
+
+
   return (
     <>
-      <div className="image-container">
-        <img src="./assets/desktop/bg-image-daytime.jpg" alt="day image" />
-      </div>
+      <main className="wrapper">
+        <img className="hero-img" src="./assets/desktop/bg-image-daytime.jpg" alt="day image" />
+          <Quote />
+          <div className="clock-wrapper">
+            <Clock />
+            <button>More <img src="./assets/desktop/icon-arrow-down.svg"/></button>
+          </div>
+        
+      </main>
     </>
   );
 }
---
+++ src/components/Clock.jsx
@@ -0,0 +1,13 @@
+
+
+
+export const Clock = () => {
+
+    return (
+        <div>
+            <p>Good morning, it's currently</p>
+            <h1>12:00 <span style={{fontSize:"56px", lineHeight:'58px'}}>BST</span></h1>
+            <p>in London, UK</p>
+        </div>
+    )
+}
